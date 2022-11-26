7890dda791fb8870e559ecbbfa6d137953cb03df
Arik Amir <aamir@liveperson.com>
2022-11-26 18:48:04 +0200

    initial commit

---
+++ Hello.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.10;
+
+contract HelloWorld {
+    string public greet = "Hello World!";
+
+    address public sender;
+    uint public amount;
+
+    function pay() external payable {
+        sender = msg.sender;
+        amount = msg.value;
+    }
+
+    function fail() external {
+        require(false, "FAILED HERE");
+         greet = "";
+    }
+}
